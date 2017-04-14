1dc79bccabd53e37162a83ea1b9356d317b8fbd2
p-hebert <philippe.hebert.qc@gmail.com>
2017-04-14 11:23:53 -0400

    GH #8 : Updated test files

--- server/tests/test.js
+++ server/tests/test.js
@@ -2,7 +2,6 @@
 
 const common = require("./common/index");
 const tests = require("./tests.json");
-const User = models.User;
 
 function importTest(name, path) {
   describe(name, function () {
@@ -19,30 +18,9 @@ describe("Futureboy Testing Suite", function () {
         importTest(tests[key].name, tests[key].path);
       }
     }
-    if(envtests.indexOf('phone') !== -1){
-      importTest("SMS Validation Code Testing", './sms/test.js');
-    }
   }else{
     for(let key in tests){
       importTest(tests[key].name, tests[key].path);
     }
   }
-
-  after(function(done){ clearUserDb(done); });
 });
-
-function clearUserDb(done){
-  var userData = {
-    email: {
-      $in: ["qtest10@abc.com"]
-    }
-  };
-
-  User.remove(userData)
-  .then(() => {
-    return User.find(userData);
-  }).then((result) => {
-    assert.equal(result.length, 0);
-    done();
-  });
-}
