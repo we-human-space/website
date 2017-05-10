2a768b90376c750e01d4e26fe4ac54492d28b967
p-hebert <philippe.hebert.qc@gmail.com>
2017-05-10 10:41:50 +0100

    GH #15 : Removed the problematic part of the code. Not an optimal solution, since it would have been cool to have persistence in our db too, but we can always transfer from the Freshmail subscriber list in the future.

--- server/src/handlers/newsletter/newsletter.js
+++ server/src/handlers/newsletter/newsletter.js
@@ -1,15 +1,12 @@
 import Ajv from 'ajv';
 import FreshMail from '../../services/mail/freshmail-js/FreshMail';
 import mongo_sanitize from 'mongo-sanitize';
-import models from '../../models';
 import config from '../../config';
 import sub_schema from './subscribe.schema.json';
 
-const Subscriber = models.Subscriber;
 const freshmail = new FreshMail(config.freshmail.api.key, config.freshmail.api.secret);
 
 export function subscribe(req, res) {
-  var subscriber;
   // Sanitizing & Validating
   Promise.resolve(mongo_sanitize(req.body))
   .then((body) => {
@@ -23,12 +20,7 @@ export function subscribe(req, res) {
       return Promise.reject(err);
     }
   }).then((validated) => {
-    subscriber = new Subscriber();
-    subscriber.email = validated.email;
-    subscriber.firstname = validated.firstname;
-    return freshmail.addSubscriber(subscriber.email, config.freshmail.newsletter.main.hash, 2, 1);
-  }).then(() => {
-    return subscriber.save();
+    return freshmail.addSubscriber(validated.email, config.freshmail.newsletter.main.hash, 2, 1);
   }).then(() => {
     res.sendStatus(200);
   }).catch((err) => {
