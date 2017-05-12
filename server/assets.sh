d8326582262998c7a5e12aab8d8956205fa69a4a
p-hebert <philippe.hebert.qc@gmail.com>
2017-05-12 15:23:16 +0100

    GH #15 : Final touch-ups to the assets.sh script

--- server/assets.sh
+++ server/assets.sh
@@ -4,6 +4,6 @@ mkdir futura;
 unzip FS-WebFonts-1034365548.zip -d futura;
 [ -d "../client/blog/assets/fonts/futura" ] && rm -rf ../client/blog/assets/fonts/futura;
 mkdir -p ../client/blog/assets/fonts/futura;
-mv futura/Fonts/* ../client/blog/assets/fonts/futura;
-mv futura/Eulas/* ../client/blog/assets/fonts/futura;
+cp -r futura/Fonts/* ../client/blog/assets/fonts/futura;
+cp -r futura/Eulas/* ../client/blog/assets/fonts/futura;
 rm -rf futura;
