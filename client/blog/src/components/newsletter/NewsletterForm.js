76c9c4fcaf4f8a76de8dc9ed520e51e2e6fbacbc
p-hebert <philippe.hebert.qc@gmail.com>
2017-05-12 15:23:39 +0100

    GH #15 : Added support to the invalid error class

--- client/blog/src/components/newsletter/NewsletterForm.js
+++ client/blog/src/components/newsletter/NewsletterForm.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-const INVALID_CLASS = 'invalid';
+const INVALID_FN_CLASS = 'firstNameError';
+const INVALID_EMAIL_CLASS = 'emailInputError';
 
 class NewsletterForm extends React.Component {
   constructor(props) {
@@ -13,7 +14,11 @@ class NewsletterForm extends React.Component {
   }
 
   handleChange(e) {
-    if(e.target.className.indexOf(INVALID_CLASS)) e.target.className.replace(INVALID_CLASS, '');
+    if(e.target.name === 'email'){
+      if(e.target.className.indexOf(INVALID_EMAIL_CLASS) !== -1) e.target.className = e.target.className.replace(INVALID_EMAIL_CLASS, '');
+    }else if(e.target.name === 'firstname'){
+      if(e.target.className.indexOf(INVALID_FN_CLASS) !== -1) e.target.className = e.target.className.replace(INVALID_FN_CLASS, '');
+    }
     this.setState({ ...(this.state), [e.target.name]: e.target.value });
   }
 
@@ -25,11 +30,11 @@ class NewsletterForm extends React.Component {
     }else{
       if(!valid.firstname){
         let input = document.querySelector('.emailForm .firstNameInput');
-        input.className += ` ${INVALID_CLASS}`;
+        input.className += ` ${INVALID_FN_CLASS}`;
       }
       if(!valid.email){
         let input = document.querySelector('.emailForm .emailInputBottom');
-        input.className += ` ${INVALID_CLASS}`;
+        input.className += ` ${INVALID_EMAIL_CLASS}`;
       }
     }
   }
