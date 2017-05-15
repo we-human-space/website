ae3e067cf25aa52ae355aefb3dcd04e22b8cd8de
p-hebert <philippe.hebert.qc@gmail.com>
2017-05-15 21:05:01 +0200

    GH #32 : Rehauled the startup scripts and package.json to support production

--- server/start.sh
+++ server/start.sh
@@ -1,2 +1,16 @@
 yarn
-yarn run dev:src
+echo "Starting M A A T server in $NODE_ENV";
+case "$NODE_ENV" in
+  "production")
+    yarn run deploy:no-test
+    ;;
+  "development")
+    yarn run dev:src
+    ;;
+  "test")
+    yarn run dev:src
+    ;;
+  *)
+    yarn run dev:src
+    ;;
+esac
