bc91f16b23c389230a4b56e2956ffb726f66fa57
p-hebert <philippe.hebert.qc@gmail.com>
2017-05-15 11:37:52 +0200

    GH #32 : Symlinks created by setup scripts were auto-recursive. Changed that and test to see if truly linked to files

--- docker/docker-start.sh
+++ docker/docker-start.sh
@@ -1,22 +1,23 @@
 # Setting Environment
 CURR_DIR=$(pwd);
 MAAT_ENV=$([ ! -z "$MAAT_ENV"  ] && echo "$MAAT_ENV" || echo "development");
-# Making the site-enabled folder
+
+echo "Creating the site-enabled folder";
 if [ ! -d "../nginx/$MAAT_ENV/sites-enabled" ]; then
   mkdir -p "../nginx/$MAAT_ENV/sites-enabled";
 fi
 
-# Setting NGINX site-enabled symbolic links
-
+echo "Setting NGINX site-enabled symbolic links";
 cd "../nginx/$MAAT_ENV/sites-available/";
 for f in *.conf ; do
   if ! [ -L "../sites-enabled/$f" ] ; then
-    ln -s "$f" "../sites-enabled/$f";
+    ln -s "$(pwd)/$f" "../sites-enabled/$f";
   fi;
 done ;
 cd "$CURR_DIR";
 
-# Start the images by building and recreating them
+
+echo "Starting the docker images by building and recreating them"
 env MAAT_ENV="$MAAT_ENV" docker-compose down && \
 env MAAT_ENV="$MAAT_ENV" docker-compose build && \
 env MAAT_ENV="$MAAT_ENV" docker-compose up --force-recreate;
