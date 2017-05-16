f2e4f861637cd7917aa828e822db4e6773bf3202
p-hebert <philippe.hebert.qc@gmail.com>
2017-05-16 10:35:55 +0200

    GH #32 : Tweaked production routing. This is currently too complex for the ROI we get out of it. Will copy development config to production instead

--- server/src/routes/production/subdomains/api/routes.js
+++ server/src/routes/production/subdomains/api/routes.js
@@ -1,18 +1,26 @@
 import express from 'express';
+import cors from 'cors';
 import renderer from '../../../../services/renderer/render';
 import handlers from '../../../../handlers/index';
+
 
 module.exports = (function() {
 
   const router = express.Router();
 
+  // const ALLOWED_ORIGINS = /.*maat\.space/;
+  // const CORS_OPTIONS = {
+  //   origin: (origin, callback) => {
+  //     if(origin.match(ALLOWED_ORIGINS)) callback(null, true);
+  //     else callback(new Error('Not allowed by CORS'));
+  //   }
+  // };
+
+  router.options('*', cors());
+
   // API Routes
 
   router.post('/feed/', handlers.feed);
-
-  router.get('/test/uploader/report', handlers.articles.test.report);
-
-  router.get('/test/uploader/clear', handlers.articles.test.clear);
 
   // 404 Catchers
 
