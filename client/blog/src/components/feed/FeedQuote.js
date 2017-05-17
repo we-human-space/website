019c1aa3cc30e57e1373ad4a7361af5df17106df
Gregory <gregorybenkoprieur@gmail.com>
2017-05-16 21:38:12 -0400

    Quote Div, Subsribe copy missing, menu 320px font and active modification

--- client/blog/src/components/feed/FeedQuote.js
+++ client/blog/src/components/feed/FeedQuote.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 export default function FeedQuote(props){
   return (
     <div className='quoteOfDay'>
-      <span className='quote320'>
-        <h2>{props.quote}</h2>
-        <span className='quoteAuthor'>{props.source}</span>
-      </span>
-      <span className='quote768'>
-        <h2>{props.quote}</h2>
-        <span className='quoteAuthor'>{props.source}</span>
-      </span>
+      <div className='quoteOfDayPlacement'>
+        <span className='quote320'>
+          <h2>{props.quote}</h2>
+          <span className='quoteAuthor'>{props.source}</span>
+        </span>
+        <span className='quote768'>
+          <h2>{props.quote}</h2>
+          <span className='quoteAuthor'>{props.source}</span>
+        </span>
+      </div>
     </div>
   );
 }
